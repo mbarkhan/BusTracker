fb0fa3a5017ea0a675c30ef316ef172fb950ef4f
Zanyar <m.berxan@gmail.com>
2023-02-16 19:01:56 +0330

    Refining Data Models/n vehicles, paths, pns_path Added /n user changed

--- auth/models/company.go
+++ auth/models/company.go
@@ -6,8 +6,11 @@ import (
 
 type Company struct {
 	gorm.Model
-
+	//Auto ID
 	Name        string  `json:"name"`
+	AdminName   string  `json:"admin_name"`
+	AdminFamily string  `jaon:"admin_family"`
+	Mobile      string  `json:"mobile" gorm:"unique"`
 	Longitude   float64 `json:"longitude"`
 	Latitude    float64 `json:"latitude"`
 	Description string  `json:"description"`
